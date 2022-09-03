4d16964a8b83b0a1edcc9524c8f213292db04589
bryce <blarkin13@gmail.com>
2022-09-02 20:06:25 -0500

    home section additions

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+
 function Home() {
     return(
         <section>
-            <h1 className="pageTitle">J&H Trucking</h1>
+            <h1 className="pageTitle">J&H Trucking LLC</h1>
+            <h2>Heading</h2>
+            <p>lorem ipsum</p>
+            <img></img>
         </section>
     )
 };
